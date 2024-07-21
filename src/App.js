ccf80705044e12f2bb3e207c663fef344586571f
밤밤 <jiiyo0@naver.com>
2024-07-21 16:42:35 +0900

    Dir: 잘못된 파일 이름 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
+import React from "react";
+import Login from "./pages/Login";
 import GlobalStyles from "./style/GlobalStyles";
+import LoginBox from "./components/LoginBox";
 
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-    </>
-  );
-main
+	return (
+		<>
+			<GlobalStyles />
+			<Login>
+				<LoginBox />
+			</Login>
+		</>
+	);
 }
 export default App;
