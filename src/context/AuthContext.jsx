c2bec8a30846559d998ca4b53110412fa211ac10
KIm Seo Young <jmh7367@naver.com>
2024-08-27 15:00:23 +0900

    Feat: loginType 상태 관리

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,30 +1,51 @@
 // AuthContext.js
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-	const [token, setToken] = useState(() => {
-		// 로컬 스토리지에서 토큰 가져오기
-		return localStorage.getItem("token") || null;
-	});
+  const [token, setToken] = useState(() => {
+    // 로컬 스토리지에서 토큰 가져오기
+    return localStorage.getItem("token") || null;
+  });
 
-	const updateToken = (newToken) => {
-		setToken(newToken);
-		if (newToken) {
-			localStorage.setItem("token", newToken); // 토큰을 로컬 스토리지에 저장
-		} else {
-			localStorage.removeItem("token"); // 로그아웃 시 토큰 제거
-		}
-	};
+  const [loginType, setLoginType] = useState(() => {
+    // 로컬 스토리지에서 토큰 가져오기
+    return localStorage.getItem("loginType") || null;
+  });
 
-	return (
-		<AuthContext.Provider value={{ token, setToken: updateToken }}>
-			{children}
-		</AuthContext.Provider>
-	);
+  const updateToken = (newToken) => {
+    setToken(newToken);
+    if (newToken) {
+      localStorage.setItem("token", newToken); // 토큰을 로컬 스토리지에 저장
+    } else {
+      localStorage.removeItem("token"); // 로그아웃 시 토큰 제거
+    }
+  };
+
+  const updateLoginType = (newLoginType) => {
+    setLoginType(newLoginType);
+    if (newLoginType) {
+      localStorage.setItem("loginType", newLoginType);
+    } else {
+      localStorage.removeItem("loginType");
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        setToken: updateToken,
+        loginType,
+        setLoginType: updateLoginType,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
-	return useContext(AuthContext);
+  return useContext(AuthContext);
 };
